b4909f2303584c475f23348092e0b07f7d7fa93e
betocantu93 <betocantu93@gmail.com>
2024-04-08 13:38:50 -0600

    fix: css for popover in pricing page

--- src/components/pricing-table/one-product/one-product.tsx
+++ src/components/pricing-table/one-product/one-product.tsx
@@ -187,8 +187,7 @@ export class OneProduct {
             </div>
 
             <span
-              class="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute left-1/2 
-    -translate-x-1/2 translate-y-full opacity-0 mx-auto bottom-0 w-10/12"
+              class="group-hover:opacity-100 transition-opacity bg-gray-800 px-1 text-sm text-gray-100 rounded-md absolute opacity-0 mx-auto -top-14 left-5 w-10/12"
             >
               <div class="p-2 w-full">
                 <span>{this.getTooltip(priceAmount, displayPriceAmount)}</span>
